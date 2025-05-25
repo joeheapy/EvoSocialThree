8a4c44b4275f6227cc22078cb45d836f5c13ac96
joeheapy <43524271+joeheapy@users.noreply.github.com>
2025-05-25 14:46:58 +0100

    Vertical compression of outcome card

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 
 # DEFAULT_PROBLEM_TEXT = "Low-income families in the United Kingdom face significant challenges accessing and affording fresh, nutritious foods. This problem creates and perpetuates health disparities, reduces quality of life, and imposes substantial long-term costs on individuals, communities, and healthcare systems."
 
-DEFAULT_PROBLEM_TEXT = "Prisons across the UK hold far more people than they were built for—around 88,000 prisoners against a “decent” capacity below 80,000. Chronic overcrowding strains safety, rehabilitation and staff, fuelling violence, re-offending and long-term public costs."
+# DEFAULT_PROBLEM_TEXT = "Prisons across the UK hold far more people than they were built for—around 88,000 prisoners against a “decent” capacity below 80,000. Chronic overcrowding strains safety, rehabilitation and staff, fuelling violence, re-offending and long-term public costs."
 
-# DEFAULT_PROBLEM_TEXT = "Thousands are forced to sleep rough or rely on temporary shelters as rents rise and welfare lags. In 2024, rough sleeping in England jumped 32%, with nearly 12,000 people on London’s streets alone, undermining health, work prospects and community cohesion."
+DEFAULT_PROBLEM_TEXT = "Thousands are forced to sleep rough or rely on temporary shelters as rents rise and welfare lags. In 2024, rough sleeping in England jumped 32%, with nearly 12,000 people on London’s streets alone, undermining health, work prospects and community cohesion."
 
 # DEFAULT_PROBLEM_TEXT = "Millions live in social homes blighted by damp, mould and structural hazards. “Awaab’s Law” now obliges landlords to investigate within 14 days, yet chronic under-investment keeps many properties sub-standard, harming residents’ health and widening inequality."
